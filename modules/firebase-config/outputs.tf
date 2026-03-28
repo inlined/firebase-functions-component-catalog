314b88e11b6a1e6a54cfa4bd67120f05093346cc
Thomas Bouldin <inlined@google.com>
2026-03-27 17:17:08 -0700

    Naming consistency

--- modules/firebase-config/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "config" {
-  description = "The Firebase admin config object."
-  value = {
-    projectId     = var.projectId
-    databaseURL   = var.databaseURL
-    storageBucket = var.storageBucket
-    location      = var.location
-  }
-}
